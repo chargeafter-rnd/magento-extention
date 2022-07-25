edf4f9f0b66ea9ebe9be37b35cffed0a58027779
Eduard Samoilenko <eduard.samoilenko@chargeafter.com>
2022-07-25 13:45:09 +0300

    Update unit-tests-setup

--- .github/scripts/unit-tests-setup.sh
+++ .github/scripts/unit-tests-setup.sh
@@ -1,2 +1,1 @@
-composer config --no-interaction allow-plugins.laminas/laminas-dependency-plugin true &&
-composer --no-interaction remove magento/composer-dependency-version-audit-plugin
+composer remove magento/composer-dependency-version-audit-plugin
