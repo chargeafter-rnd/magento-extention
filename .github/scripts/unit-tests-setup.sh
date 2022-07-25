de802d6f2f1fbeaac118e842aeaea23e792f2136
Eduard Samoilenko <eduard.samoilenko@chargeafter.com>
2022-07-25 12:20:17 +0300

    Add multiple command

--- .github/scripts/unit-tests-setup.sh
+++ .github/scripts/unit-tests-setup.sh
@@ -1,2 +1,2 @@
-composer remove magento/composer-dependency-version-audit-plugin
+composer remove magento/composer-dependency-version-audit-plugin &&
 composer config --no-plugins allow-plugins.laminas/laminas-dependency-plugin true
