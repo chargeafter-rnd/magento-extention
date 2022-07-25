9a1c2816169e1d505a52f95e5081438ba0ee0595
Eduard Samoilenko <eduard.samoilenko@chargeafter.com>
2022-07-25 12:28:52 +0300

    Update composer script

--- .github/scripts/unit-tests-setup.sh
+++ .github/scripts/unit-tests-setup.sh
@@ -1,2 +1,2 @@
 composer remove magento/composer-dependency-version-audit-plugin &&
-composer config --no-plugins allow-plugins.laminas/laminas-dependency-plugin true
+composer config --no-interaction allow-plugins.laminas/laminas-dependency-plugin true
