a7353a3113c807e761c5f1d62504346032674adf
Eduard Samoilenko <eduard.samoilenko@chargeafter.com>
2022-07-25 13:23:11 +0300

    Update composer config

--- .github/scripts/unit-tests-setup.sh
+++ .github/scripts/unit-tests-setup.sh
@@ -1,2 +1,2 @@
-composer remove magento/composer-dependency-version-audit-plugin &&
-composer global config --no-plugins allow-plugins.laminas/laminas-dependency-plugin true
+composer config --no-plugins allow-plugins.laminas/laminas-dependency-plugin true
+composer remove magento/composer-dependency-version-audit-plugin
