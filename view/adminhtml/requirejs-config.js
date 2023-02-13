3eed733d69bac38528076df7633d7adacb69d0d4
Eduard Samoilenko <eduard.samoilenko@chargeafter.com>
2023-02-13 11:33:08 +0200

    Update widget types. Add form validation

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'mage/validation': {
+                'Chargeafter_Payment/js/admin-config/validator-rules-mixin': true
+            }
+        }
+    }
+};
